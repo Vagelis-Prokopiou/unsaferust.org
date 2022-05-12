504c751b486f2ce7a706f40be4ac57fbdeba78fe
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2022-05-12 20:22:47 +0300

    Fixing footer text alignment in mobile dimension

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -8,15 +8,8 @@ const Footer: () => JSX.Element = () => {
             <div
                 class="container flex flex-col items-center px-4 py-6 mx-auto space-y-6 lg:space-y-0 lg:flex-row lg:justify-between">
                 <div class="w-48 text-gray-700 dark:text-gray-200">
-                    <div class="flex items-center">
-                        {/*<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 27"*/}
-                        {/*     class="h-7 w-7 -mt-2 flex-shrink-0">*/}
-                        {/*    <path*/}
-                        {/*        d="M22.487.658s5.03 13.072-1.822 22.171C16.476 28.39 9.84 27.26 5.484 25.68c1.513-3.391 3.441-6.067 5.784-8.03 1.176.623 3.186.792 6.03.51-2.535-.221-4.284-.654-5.246-1.3l.125.08c2.122-1.546 4.556-2.556 7.303-3.029-3.16-.285-6.026.315-8.598 1.804-.577-.742-1.157-1.748-1.74-3.018.07 1.534.339 2.734.809 3.6-2.64 1.797-4.953 4.58-6.94 8.351a7.583 7.583 0 01-.188-.088c-.802-.358-1.328-1.037-1.755-2.036C-1.9 13.366 4.645 8.273 11.123 7.989 23.046 7.465 22.487.658 22.487.658z"*/}
-                        {/*        fill="#0ED3CF" data-darkreader-inline-fill=""*/}
-                        {/*        style="--darkreader-inline-fill:#3bf2ee;"></path>*/}
-                        {/*</svg>*/}
-                        <p class="text-gray-500 dark:text-gray-300">unsaferust.org</p>
+                    <div>
+                        <p class="text-center lg:text-left text-gray-500 dark:text-gray-300">unsaferust.org</p>
                     </div>
                 </div>
                 <p class="text-sm text-gray-500 dark:text-gray-300">{(new Date().getFullYear())}. All Rights
